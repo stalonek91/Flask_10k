d6f9cba6d5df42a10e7bf0db62675d463fda4c3f
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2024-01-27 12:26:54 +0100

    DB creation works

--- app.py
+++ app.py
@@ -4,6 +4,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///flask_10k.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_pyfile('config.cfg')
+
 db = SQLAlchemy(app)
+
+class Lesson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Float, nullable=False)
+    content = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f'Lesson ID: {self.id} time spent:{self.time}, content: {self.content}'
+
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+
+if __name__ == '__main__':
+    create_tables()
+    app.run(debug=True)
