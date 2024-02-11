2aea9af1d5afd680c3d23ea8c90a99a2a2dd54ef
Sylwester Sojka <sylwestersojka@Sylwesters-MacBook-Air.local>
2024-02-11 20:34:35 +0100

    Working register function

--- app.py
+++ app.py
@@ -4,10 +4,12 @@
 #TODO: generic -> add error handling to databases or basiaclly in forms, try except
 #TODO: add only 5 last updates on top of page not full list
 
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from forms import LoginForm
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_required
+from urllib.parse import urlparse, urljoin
 
 
 
@@ -17,12 +19,33 @@ app.config.from_pyfile('config.cfg')
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'Log in first to acess the tracker!'
+
+
+def is_safe_url(target):
+    ref_url = urlparse(request.host_url)
+    test_url = urlparse(urljoin(request.host_url, target))
+    return test_url.scheme in ('http', 'https') and ref_url.netloc == test_url.netloc
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     id = db.Column(db.Integer, unique=True, primary_key=True)
     username = db.Column(db.String(25), nullable=False)
     email = db.Column(db.String(254),unique=True, nullable=False)
     password = db.Column(db.String(30), nullable=False)
+
+    def set_password(self, password):
+        self.password = bcrypt.generate_password_hash(password).decode('utf-8')
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
 
 
 
@@ -89,14 +112,17 @@ def index():
 
 
 @app.route('/content')
+@login_required
 def content():
-    lessons = Lesson.query.all()
+    lessons = Lesson.query.order_by(Lesson.id.desc()).limit(5).all()
     time_left = update_time()
 
     time_to_display = int(time_left) if time_left %1 == 0 else time_left
     print(f'Time to display: {time_to_display}')
 
     return render_template('content.html', lessons=lessons, time_to_display=time_to_display)
+
+
 
 #FIXME: hash pass + flash messages
 @app.route('/login', methods=['POST', 'GET'])
@@ -105,13 +131,23 @@ def login():
     form = LoginForm()
     form.submit_button.label.text = f'{title}'
 
-    if form.validate_on_submit():
-        print(f'Submitted {title} form ')
-        return 'kwoka'
+    if request.method == 'POST':
+        
+        username = form.username.data
+        password = form.password.data
+
+        user = User.query.filter_by(username=username)
+
+
+        if form.validate_on_submit():
+            print(f'Submitted {title} form ')
+            return 'kwoka'
+    
+    session['next'] = request.args.get('next')
     return render_template('login.html', title=title, form = form)
 
 
-#FIXME: flash messages
+#FIXME: flash messages using 'flash' module
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     title = 'Register'
@@ -121,17 +157,15 @@ def register():
     if form.validate_on_submit():
         print(f'Submitted {title} form ')
 
-        username = form.username.data
-        email = form.email.data
-        plain_password = form.password.data
+        new_user = User(username=form.username.data, email=form.email.data)
+        new_user.set_password(form.password.data)
 
-        hashed_password = bcrypt.generate_password_hash(plain_password).decode('utf-8')
 
-        new_user = User(username=username, email=email, password=hashed_password)
         db.session.add(new_user)
         db.session.commit()
 
-        return f'Success! user: {username} has been created!'
+        flash(f'You have been registered: {new_user.username}')
+        return redirect(url_for('login'))
        
         
     return render_template('register.html', form = form, title = title)
